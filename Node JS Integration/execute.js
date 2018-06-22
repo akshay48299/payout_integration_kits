a1d850d06001e0dc137a232dde8fb06f9bc9f67d
preetha-in <40459012+preetha-in@users.noreply.github.com>
2018-06-22 17:35:03 +0530

    Added Node JS Integration Code

---
+++ Node JS Integration/execute.js
@@ -0,0 +1,11 @@
+const user = require('./cfPayout.js');
+
+let user2 = new user;
+
+user2.clientAuth('CF27D9CZCLC0ZHYUE26','b4c83b231adae60400ce303361ecadeacc004916',"TEST")
+user2.expiryCheck()
+user2.addBeneficiary('JOHN180121','john doe', 'johndoe@cashfree.com', '9876543210','00091111202233','HDFC0000001','vpa','ABC Street','add 2','Bangalore', 'Karnataka','560001' )
+user2.requestTransfer('JOHN18011','100','76723288672267867867','banktransfer','optional')
+user2.getTransferStatus('76723288672267867867')
+user2.bankDetailsValidation("Joh",'9910115208', '00011020001772', 'HDFC0000001')
+user2.getBalance()
